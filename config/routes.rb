cc6e355e27cb30169d283001a0158b594c155856
Hoon Lee <yl.hoony@gmail.com>
2018-01-24 13:30:40 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts, only: [:index, :show, :new, :create]
+  patch 'posts/:id', to: 'posts#update'
 end
